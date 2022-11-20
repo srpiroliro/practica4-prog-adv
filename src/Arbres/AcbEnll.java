79c7cc988072ca7a0e1fea263c18cacd67bb450b
srpiroliro <srpiroliro21@gmail.com>
2022-11-20 14:19:18 +0100

    update

--- src/Arbres/AcbEnll.java
+++ src/Arbres/AcbEnll.java
@@ -45,11 +45,13 @@ public class AcbEnll<E extends Comparable<E>> implements Acb<E>{
         NodeA(E e){this(e,null,null);}
         NodeA(E e, NodeA l, NodeA r){inf=e; esq=l; dret=r;}
 
+        // compareTo(einf) or just einf-inf
+
         private void inserir(E einf) throws ArbreException{
-            if(einf.compareTo(inf)<0){
+            if(compareTo(einf)<0){
                 if(esq!=null) esq.inserir(einf);
                 else esq=new NodeA(einf);
-            } else if (einf.compareTo(inf)>0) {
+            } else if(compareTo(einf)>0){
                 if(dret!=null) dret.inserir(einf);
                 else dret=new NodeA(einf);
             } else throw new ArbreException("element ja hi es");
@@ -60,15 +62,15 @@ public class AcbEnll<E extends Comparable<E>> implements Acb<E>{
                 if(esq!=null){
                     esq=esq.esborrar(einf); return this;
                 } else throw new ArbreException("no hi es");
-            } else if (compareTo(einf)>0){
+            }else if(compareTo(einf)>0){
                 if(dret!=null){
                     dret=dret.esborrar(einf); return this;
                 } else throw new ArbreException("no hi es");
-            } else if(esq!=null&&dret!=null){
-                inf=dret.buscarMinim();
+            }else if(esq!=null&&dret!=null){
+                inf=dret.buscarMinim(); // ???? pq dret
                 dret=dret.esborrar(inf);
                 return this;
-            } else return(esq==null && dret==null)?null:((esq==null)?dret:esq);
+            }else return(esq==null&&dret==null)?null:((esq==null)?dret:esq);
         }
 
         private boolean hiEs(E einf){
@@ -91,9 +93,28 @@ public class AcbEnll<E extends Comparable<E>> implements Acb<E>{
             if(a2!=null) a2.inordre(sentit, c);
         }
 
+        // CHECK: necessary?
         // @Override
         public int compareTo(E node) { // CHECK (check for correctnes + test)
             return (int)inf-(int)((NodeA)node).inf;
+        }
+
+        private Object cloner(){ // CHECK
+            // "No es permet invocar a cap mètode de la classe en la seva implementació." ??????
+            NodeA copia=null;
+            try{
+                copia=(NodeA) super.clone();
+                if(esq!=null)  copia.esq=(NodeA)  esq.cloner();
+                if(dret!=null) copia.dret=(NodeA) dret.cloner();
+            }catch(CloneNotSupportedException e){}
+            return copia;
+        }
+
+        private int cardinalitat(){
+            int h=0;
+            if(esq!=null) h+=esq.cardinalitat();
+            if(dret!=null) h+=dret.cardinalitat();
+            return 1+h;
         }
     }
     private Queue<E> cua=null;
@@ -105,22 +126,25 @@ public class AcbEnll<E extends Comparable<E>> implements Acb<E>{
             cua=new LinkedList<>();
             arrel.inordre(sentit, cua);
         }
-
     }
 
     public boolean finalRecorregut(){
-        /* ‐ la darrera vegada que es va invocar segRecorregut aquest mètode  
-            ja va retornar el darrer element en inordre de l’arbre.                 ???????????? */
+        /* "la darrera vegada que es va invocar segRecorregut aquest mètode  
+            ja va retornar el darrer element en inordre de l’arbre."                ???????????? */
 
         return cua.isEmpty();
     }
 
     public E segRecorregut() throws ArbreException {
-        if(cua==null||finalRecorregut()) 
-            throw new ArbreException("Cal iniciar recorregut!");
-        E element=cua.peek(); // gets first element (returns null if there isnt one) 
-        cua.remove(); // removes first element
+        if(cua==null||finalRecorregut()) throw new ArbreException("Cal iniciar recorregut!");
+        E element=cua.peek(); cua.remove();
         return element;
+    }
+
+    public Object clone(){return arrel.cloner();}
+    public int cardinalitat(){
+        // iniRecorregut -> cua.size -> cua=old_cua
+        return arrel.cardinalitat();
     }
 
 }
